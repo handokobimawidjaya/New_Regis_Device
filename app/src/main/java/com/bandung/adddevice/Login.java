c42d178746d7bd5bd2d8aa701bbb95ef53d2e69b
handokobimawidjaya <handoko.16421007@student.ubl.ac.id>
2020-03-02 11:16:17 +0700

    Hello

--- app/src/main/java/com/bandung/adddevice/Login.java
+++ app/src/main/java/com/bandung/adddevice/Login.java
@@ -10,8 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.bandung.adddevice.Session.SharedPrefManager;
 import com.bandung.adddevice.Support.InitRetrofit;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,6 +22,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,82 +32,93 @@ import retrofit2.Response;
 
 public class Login extends AppCompatActivity {
 
-    private LinearLayout coordinatorLayout;
-    EditText edtEmail, edtPassword;
-    Button Login;
+    SharedPrefManager sharedPrefManager;
+    EditText email;
+    EditText password;
+//    @BindView(R.id.btnLogin)
+    Button btnLogin;
+    @BindView(R.id.relativ)
+    RelativeLayout coordinatorLayout;
     ProgressDialog loading;
+
+    String name, name2, macSensor;
+    Boolean session = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        sharedPrefManager = new SharedPrefManager(this);
 
-        edtEmail= (EditText)findViewById( R.id.username );
-        edtPassword=(EditText)findViewById( R.id.password );
+        email = (EditText) findViewById( R.id.email );
+        password = (EditText) findViewById( R.id.password );
 
-        Login=(Button)findViewById(R.id.btnLogin);
-
-        Login.setOnClickListener(new View.OnClickListener() {
+        btnLogin = (Button) findViewById( R.id.btnLogin );
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (edtEmail.getText().toString().equals("") || edtPassword.getText().toString().equals("")) {
-                    edtEmail.setFocusable(false);
-                    edtPassword.setFocusable(false);
-                    showSnackbar();
-                } else {
-                    loading = ProgressDialog.show(Login.this,"Loading.....",null,true,true);
-                    RequestLogin();
-                }
+            public void onClick(View view) {
+                Check();
             }
         });
-//        if (sharedPrefManager.getSudahLogin()){
-//            startActivity(new Intent(Login.this, Dashboard.class)
-//                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
-//            finish();
-//        }
+        if (sharedPrefManager.getSudahLogin()){
+            startActivity(new Intent(Login.this, MainActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        }
+
     }
 
-    private void RequestLogin(){
-        String user = String.valueOf(edtEmail.getText());
-        String pas = String.valueOf(edtPassword.getText());
+    private void Check() {
+        if (email.getText().toString().equals("") || password.getText().toString().equals("")) {
+            email.setFocusable(false);
+            password.setFocusable(false);
+            showSnackbar();
+        } else {
+            loading = ProgressDialog.show(Login.this,"Loading.....",null,true,true);
+            RequestLogin();
+        }
+
+    }
+
+    private void RequestLogin() {
+        String user = String.valueOf(email.getText());
+        String pas = String.valueOf(password.getText());
         if (user.equals("")) {
             showSnackbar();
         } else if (pas.equals("")) {
             showSnackbar();
         } else {
-//            mApiService.userLogin(Username.getText().toString(), Password.getText().toString())
             retrofit2.Call<ResponseBody> call = InitRetrofit.getInstance().getApi().userLogin(user,pas);
             call.enqueue(new Callback<ResponseBody>() {
                 @Override
                 public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                     if (response.isSuccessful()){
                         loading.dismiss();
-//                        try {
-//                            JSONObject jsonRESULTS = new JSONObject(response.body().string());
-//                            if (jsonRESULTS.getString("error").equals("false")){
-//                                Log.d("response api", jsonRESULTS.toString());
-//                                Toast.makeText(Login.this, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
-////                                        String id = jsonRESULTS.getJSONObject("user").getString("ID");
-//                                String nama = jsonRESULTS.getJSONObject("user").getString("nama");
-//                                String email = jsonRESULTS.getJSONObject("user").getString("email");
-//                                Log.d("alamat", nama+email.toString());
-                                startActivity(new Intent(Login.this, MainActivity.class));
-//                                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                        try {
+                            JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                            if (jsonRESULTS.getString("status").equals("true")){
+                                Log.d("response api", jsonRESULTS.toString());
+                                String json=jsonRESULTS.getString("app");
+                                Toast.makeText(Login.this, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+
+                                sharedPrefManager.saveSPBoolean( SharedPrefManager.SP_SUDAH_LOGIN, true);
+                                startActivity(new Intent(Login.this, MainActivity.class)
+                                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
                                 finish();
-//                            } else {
-//                                String error_message = jsonRESULTS.getString("error_msg");
-//                                Toast.makeText(Login.this, error_message, Toast.LENGTH_SHORT).show();
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        } catch (IOException e) {
-//                            e.printStackTrace();
-//                        }
-//                    } else {
-//                        loading.dismiss();
+                            } else {
+                                String error_message = jsonRESULTS.getString("error_msg");
+                                Toast.makeText(Login.this, error_message, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        loading.dismiss();
                     }
                 }
-
                 @Override
                 public void onFailure(Call<ResponseBody> call, Throwable t) {
                     Log.v("debug", "onFailure: ERROR > " + t.toString());
@@ -111,20 +127,19 @@ public class Login extends AppCompatActivity {
                 }
             });
         }
-
-}
+    }
 
     public void showSnackbar() {
-        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Lengkapi data terlebih dahulu", Snackbar.LENGTH_INDEFINITE)
+        Snackbar snackbar = Snackbar.make(coordinatorLayout, "Harus Di isi !!!", Snackbar.LENGTH_INDEFINITE)
                 .setAction("Ulangi", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Snackbar snackbar1 = Snackbar.make(coordinatorLayout, "Silahkan ulangi", Snackbar.LENGTH_SHORT);
                         snackbar1.show();
-                        edtEmail.setFocusableInTouchMode(true);
-                        edtPassword.setFocusableInTouchMode(true);
+                        email.setFocusableInTouchMode(true);
+                        password.setFocusableInTouchMode(true);
                     }
                 });
         snackbar.show();
-}
+    }
 }
--- app/src/main/java/com/bandung/adddevice/Support/InitRetrofit.java
+++ app/src/main/java/com/bandung/adddevice/Support/InitRetrofit.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InitRetrofit {
-    public static final String BASE_URL = "http://192.168.14.15:3000/";
+    public static final String BASE_URL = "http://192.168.14.11:3000/";
     private static InitRetrofit mInstance;
     private Retrofit retrofit;
 
